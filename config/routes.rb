287f621b846fb6e81d804ee70bd6faa7a9d8a869
andela-ydaniju <yusuf.daniju@andela.com>
2016-06-06 13:58:47 +0100

    add generate devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 CheckSmarterApi::Application.routes.draw do
+  devise_for :users
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
       resources :users, except: [:index] do
