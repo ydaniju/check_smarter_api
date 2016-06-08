8144d5ec8bf60c64ed5a41136053c83d3af33f56
andela-ydaniju <yusuf.daniju@andela.com>
2016-06-08 08:12:01 +0100

    deploy to  heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 class HomeController < ApplicationController
   def index
-    render json: { success: "Welcome home. Visit
-      https://github.com/andela-ydaniju/check_smarter_api for documentation" }
   end
 end
---
+++ app/views/home/index.json.jbuilder
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+json.welcome "Welcome to CheckSmarter. Visit https://github.com/"\
+"andela-ydaniju/check_smarter_api for documentation"
